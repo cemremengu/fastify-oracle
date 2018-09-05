372c3d337634c296d8ac404bd0b193c8fda60166
cemremengu <cemre.mengu@ttgint.com>
2018-09-05 16:27:11 +0300

    Added multiple pool support

--- plugin.js
+++ plugin.js
@@ -3,26 +3,61 @@
 const fp = require('fastify-plugin')
 const oracledb = require('oracledb')
 
-function fastifyOracleDB (fastify, options, next) {
-  const close = function (fastify, done) {
-    fastify.oracle.close(done)
+const close = function (fastify, done) {
+  Object.keys(fastify.oracle)
+    .forEach(key => {
+      if (fastify.oracle[key].pool) {
+        fastify.oracle[key].pool.close(done)
+      }
+    })
+
+  if (fastify.oracle.pool) {
+    fastify.oracle.pool.close(done)
+  }
+}
+
+function decorateFastifyInstance(pool, fastify, options, next) {
+  const oracle = {
+    getConnection: pool.getConnection.bind(pool),
+    pool
+  };
+
+  if (options.name) {
+    if (!fastify.oracle) {
+      fastify.decorate('oracle', Object.assign(oracle, { db: oracledb }))
+    }
+
+    if (fastify.oracle[options.name]) {
+      return next(new Error('Connection name has already been registered: ' + options.name))
+    }
+
+    fastify.oracle[options.name] = oracle
+  } else {
+    if (fastify.oracle) {
+      return next(new Error('fastify-oracle has already been registered'))
+    }
+    else {
+      fastify.decorate('oracle', Object.assign(oracle, { db: oracledb }))
+    }
   }
 
+  fastify.addHook('onClose', close)
+
+  return next()
+}
+
+function fastifyOracleDB(fastify, options, next) {
   if (options.client) {
     if (oracledb.Pool.prototype.isPrototypeOf(options.client) === false) {
       return next(Error('supplied client must be an instance of oracledb.pool'))
     }
-    fastify.decorate('oracle', options.client)
-    fastify.addHook('onClose', close)
-    return next()
+    return decorateFastifyInstance(options.client, fastify, options, next)
   }
 
   if (options.poolAlias) {
     const pool = oracledb.getPool(options.poolAlias)
-    if (!pool) return next('could not get default pool from oracledb instance')
-    fastify.decorate('oracle', pool)
-    fastify.addHook('onClose', close)
-    return next()
+    if (!pool) return next(Error('could not get default pool from oracledb instance'))
+    return decorateFastifyInstance(pool, fastify, options, next)
   }
 
   if (!options.pool) {
@@ -31,9 +66,7 @@ function fastifyOracleDB (fastify, options, next) {
 
   oracledb.createPool(options.pool, (err, pool) => {
     if (err) return next(err)
-    fastify.decorate('oracle', pool)
-    fastify.addHook('onClose', close)
-    next()
+    return decorateFastifyInstance(pool, fastify, options, next)
   })
 }
 
