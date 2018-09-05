e3a095961f74bd0a8cc8da05081e8c7c533347bb
cemremengu <cemre.mengu@ttgint.com>
2018-09-05 22:22:48 +0300

    Added more tests

--- test/pool.test.js
+++ test/pool.test.js
@@ -3,7 +3,7 @@
 const test = require('tap').test
 const plugin = require('../plugin')
 
-test('creates usable pool from config', (t) => {
+test('creates pool from config', (t) => {
   t.plan(6)
 
   const fastify = {
@@ -28,10 +28,59 @@ test('creates usable pool from config', (t) => {
     t.ok(fastify.oracle)
     fastify.oracle.getConnection()
       .then((conn) => {
-        conn.execute('select 1 from dual')
+        conn.execute('SELECT 1 AS FOO FROM DUAL', { }, { outFormat: fastify.oracle.db.OBJECT })
           .then((result) => {
             t.is(result.rows.length, 1)
-            t.is(result.rows[0][0], 1)
+            t.is(result.rows[0].FOO, 1)
+          })
+          .then(() => conn.close())
+          .catch(t.threw)
+      })
+      .catch(t.threw)
+  })
+})
+
+test('creates named pool from config', (t) => {
+  t.plan(8)
+
+  const fastify = {
+    decorate (name, obj) {
+      t.is(name, 'oracle')
+      this[name] = obj
+    },
+
+    addHook (name, fn) {
+      t.is(name, 'onClose')
+      t.match(fn, /fastify\.oracle\.pool\.close/)
+    }
+  }
+
+  const opts = {
+    user: 'travis',
+    password: 'travis',
+    connectString: 'localhost/xe'
+  }
+  plugin(fastify, { pool: opts, name: 'testdb' }, (err) => {
+    if (err) t.threw(err)
+    t.ok(fastify.oracle)
+    fastify.oracle.getConnection()
+      .then((conn) => {
+        conn.execute('SELECT 1 AS FOO FROM DUAL', { }, { outFormat: fastify.oracle.db.OBJECT })
+          .then((result) => {
+            t.is(result.rows.length, 1)
+            t.is(result.rows[0].FOO, 1)
+          })
+          .then(() => conn.close())
+          .catch(t.threw)
+      })
+      .catch(t.threw)
+
+    fastify.oracle.testdb.getConnection()
+      .then((conn) => {
+        conn.execute('SELECT 1 AS FOO FROM DUAL', { }, { outFormat: fastify.oracle.db.OBJECT })
+          .then((result) => {
+            t.is(result.rows.length, 1)
+            t.is(result.rows[0].FOO, 1)
           })
           .then(() => conn.close())
           .catch(t.threw)
